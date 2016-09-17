f62767e1c50466ebebf9eec77ce0c5dbedf09e7d
cathens <cathens@iastate.edu>
2016-09-17 14:46:45 -0500

    Developed functionality for next and back buttons

--- app/src/main/java/bitsplease/ourstate/PetitionListActivity.java
+++ app/src/main/java/bitsplease/ourstate/PetitionListActivity.java
@@ -117,6 +117,27 @@ public class PetitionListActivity extends AppCompatActivity {
                 startActivity(intentProfile);
             }
         });
+
+        Button buttonBack = (Button) findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                leftSideDecrement();
+//                Intent intentProfile = new Intent(PetitionListActivity.this, PetitionDescActivity.class);
+//                startActivity(intentProfile);
+            }
+        });
+
+        Button buttonNext = (Button) findViewById(R.id.buttonNext);
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                leftSideIncrement();
+//                Intent intentProfile = new Intent(PetitionListActivity.this, PetitionDescActivity.class);
+//                startActivity(intentProfile);
+            }
+        });
+
     };
 
     private void leftSideInitialize(){
