c32e108e55368e5730433bd26ae5c67148786e3b
Armandh96 <armandh@iastate.edu>
2016-09-17 09:31:37 -0500

    fixed loginactivity

---
+++ app/src/main/java/bitsplease/ourstate/User.java
@@ -0,0 +1,17 @@
+package bitsplease.ourstate;
+
+/**
+ * Created by Armand on 9/17/2016.
+ */
+public class User {
+    public String userId;
+    public String username;
+    public String password;
+
+    public User(String userId,String username,String password)
+    {
+        this.userId=userId;
+        this.username=username;
+        this.password=username;
+    }
+}
