ad9f8c4711e475e97a8074d69c3120aca25848a8
cathens <cathens@iastate.edu>
2016-09-17 01:50:09 -0500

    Added petition constructor, getter/setter methods, and incrementVotes method.

--- app/src/main/java/bitsplease/ourstate/Petition.java
+++ app/src/main/java/bitsplease/ourstate/Petition.java
@@ -3,7 +3,79 @@ package bitsplease.ourstate;
 /**
  * Created by claudiaathens on 9/17/16.
  */
+
 public class Petition {
 
-    
+    private int id;
+    private String petitionTitle;
+    private String petitionDesc;
+    private int votes;
+    private String creator;
+    private int creationTime;
+    private DatabaseHelper db;
+
+    public Petition(int id, String petitionTitle, String petitionDesc, int votes, String creator, int creationTime) {
+        this.id = id;
+        this.petitionTitle = petitionTitle;
+        this.petitionDesc = petitionDesc;
+        this.votes = votes;
+        this.creator = creator;
+        this.creationTime = creationTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPetitionTitle() {
+        return petitionTitle;
+    }
+
+    public void setPetitionTitle(String petitionTitle) {
+        this.petitionTitle = petitionTitle;
+    }
+
+    public String getPetitionDesc() {
+        return petitionDesc;
+    }
+
+    public void setPetitionDesc(String petitionDesc) {
+        this.petitionDesc = petitionDesc;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+
+    public void incrementVote(int votes){
+        votes++;
+        db.petitionVote(id);
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public int getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(int creationTime) {
+        this.creationTime = creationTime;
+    }
+
+
+
 }
