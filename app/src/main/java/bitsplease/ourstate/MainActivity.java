327ff7168307bc10e5ab8b4f788aacf6bbd0345f
cathens <cathens@iastate.edu>
2016-09-17 08:35:04 -0500

    Created profile button functionality

--- app/src/main/java/bitsplease/ourstate/MainActivity.java
+++ app/src/main/java/bitsplease/ourstate/MainActivity.java
@@ -2,6 +2,10 @@ package bitsplease.ourstate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,16 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button button1 = (Button) findViewById(R.id.profileButton);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentProfile = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intentProfile);
+            }
+        });
 
-
-    }
-
-//    Button button = (Button) findViewById(R.id.my_button);
-//    button.setOnClickListener() {
-//        @Override
-//        public void onClick(View view) {
-//            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-//            startActivity(intent);
-//        }
-//    };
+    };
 }
