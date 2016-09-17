aa0a82093f6c42e7ce5dfdc58b1cd0bb4ce72a5f
Zach Knoll <zgknoll@iastate.edu>
2016-09-17 09:33:37 -0500

    Added getters and setters

--- app/src/main/java/bitsplease/ourstate/User.java
+++ app/src/main/java/bitsplease/ourstate/User.java
@@ -14,4 +14,28 @@ public class User {
         this.username=username;
         this.password=username;
     }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
