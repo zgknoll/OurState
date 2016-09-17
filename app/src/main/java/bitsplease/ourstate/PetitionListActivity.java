240d40045bca862f73c8bb9f183f9bbf61bced39
cathens <cathens@iastate.edu>
2016-09-17 10:40:21 -0500

    PetitionListActivity

--- app/src/main/java/bitsplease/ourstate/PetitionListActivity.java
+++ app/src/main/java/bitsplease/ourstate/PetitionListActivity.java
@@ -2,6 +2,9 @@ package bitsplease.ourstate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+
 
 public class PetitionListActivity extends AppCompatActivity {
 
@@ -10,4 +13,8 @@ public class PetitionListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_petition_list);
     }
+
+    //setContentView(R.layout.layoutName);
+    TextView textView1 = (TextView) findViewById(R.id.textView);
+    //textView1.setText("text you want to display");
 }
