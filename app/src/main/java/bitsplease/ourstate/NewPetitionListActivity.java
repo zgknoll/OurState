649380e754a33e6adcfa5f08a0a71229b47d05e5
Armandh96 <armandh@iastate.edu>
2016-09-18 07:32:45 -0500

    Fixed some shit

--- app/src/main/java/bitsplease/ourstate/NewPetitionListActivity.java
+++ app/src/main/java/bitsplease/ourstate/NewPetitionListActivity.java
@@ -10,28 +10,70 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 
 
 public class NewPetitionListActivity extends Activity {
 
+    private int count;
+//    private Petition[] petitionArray;
+//    private DatabaseHelper db;
+//    private int petCount;
+//    private int voteCount;
+//    private int timeCount;
+//    private long currentTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        db=new DatabaseHelper(this,1);
+//        petitionArray= db.getAllPetitions();
         setContentView(R.layout.activity_new_petition_list);
 
         ArrayList<String> petitionsList = new ArrayList<String>();
+        count=petitionsList.size();
+//        petCount=petitionsList.size();
+//        voteCount=petitionsList.size();
+//        timeCount=petitionsList.size();
+//        while(petitionArray.length>petCount)
+//        {
+//            petitionsList.add(petitionArray[petCount].getPetitionTitle());
+//            petCount++;
+//        }
+
         petitionsList.add("Petition 1");
         petitionsList.add("Petition 2");
         petitionsList.add("Petition 3");
+        petitionsList.add("Petition 4");
+        petitionsList.add("Petition 5");
         ArrayList<String> petitionNumList = new ArrayList<String>();
-        petitionNumList.add("1.");
-        petitionNumList.add("2.");
-        petitionNumList.add("3.");
+        while(petitionsList.size()>count)
+        {
+            petitionNumList.add(count+1 + ".");
+            count++;
+
+        }
         ArrayList<String> petitionVotesList = new ArrayList<String>();
+//        while(petitionArray.length>voteCount)
+//        {
+//            petitionVotesList.add(petitionArray[voteCount].getVotes()+"votes");
+//            voteCount++;
+//        }
         petitionVotesList.add("5 votes");
         petitionVotesList.add("10 votes");
         petitionVotesList.add("15 votes");
+        petitionVotesList.add("20 votes");
+        petitionVotesList.add("25 votes");
         ArrayList<String> timeAgoList = new ArrayList<String>();
+//        while(petitionArray.length>timeCount)
+//        {
+//            currentTime=new Date().getTime();
+//            petitionVotesList.add(TimeAgo.toDuration(currentTime-(petitionArray[timeCount].getCreationTime())));
+//            timeCount++;
+//        }
+        timeAgoList.add(TimeAgo.toDuration(1230000000));
+        timeAgoList.add(TimeAgo.toDuration(1230000000));
         timeAgoList.add(TimeAgo.toDuration(1230000000));
         timeAgoList.add(TimeAgo.toDuration(1230000000));
         timeAgoList.add(TimeAgo.toDuration(1230000000));
