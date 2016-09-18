0720879479086390c0bb058c99259852cfebdd86
Armandh96 <armandh@iastate.edu>
2016-09-18 02:53:39 -0500

    Fixed some stuff

--- app/src/main/java/bitsplease/ourstate/CustomAdapter.java
+++ app/src/main/java/bitsplease/ourstate/CustomAdapter.java
@@ -5,7 +5,73 @@ package bitsplease.ourstate;
  */
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-public class CustomAdapter {
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class CustomAdapter extends BaseAdapter{
+
+    String[] petitions;
+    String[] petitionVotes;
+    String[] petitionNum;
+    String[] timeAgo;
+    Context context;
+    private static LayoutInflater petitionInflater=null;
+
+    public CustomAdapter(Context context,String[] petitions,String[] petitionVotes,String[] petitionNum,String[] timeAgo ){
+       // super(context,R.layout.custom_row,petitions);//,petitionNums,petitionVotes,timeAgo);
+        this.petitions=petitions;
+        this.petitionVotes=petitionVotes;
+        this.petitionNum=petitionNum;
+        this.timeAgo=timeAgo;
+        this.context=context;
+        petitionInflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return petitions.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    public class Holder
+    {
+        TextView petitionName;
+        TextView petitionNum;
+        TextView petitionVotes;
+        TextView timeAgo;
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        Holder holder= new Holder();
+        View rowView;
+        rowView=petitionInflater.inflate(R.layout.custom_row,null);
+        holder.petitionName=(TextView)rowView.findViewById(R.id.petitionName);
+        holder.petitionNum=(TextView)rowView.findViewById(R.id.petitionNum);
+        holder.petitionVotes=(TextView)rowView.findViewById(R.id.numberVotes);
+        holder.timeAgo=(TextView)rowView.findViewById(R.id.timeAgo);
+        holder.petitionName.setText(petitions[position]);
+        holder.petitionNum.setText(petitionNum[position]);
+        holder.petitionVotes.setText(petitionVotes[position]);
+        holder.timeAgo.setText(timeAgo[position]);
+        return rowView;
+    }
 }
--- app/src/main/java/bitsplease/ourstate/NewPetitionListActivity.java
+++ app/src/main/java/bitsplease/ourstate/NewPetitionListActivity.java
@@ -9,6 +9,8 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 
 public class NewPetitionListActivity extends Activity {
 
@@ -17,10 +19,33 @@ public class NewPetitionListActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_petition_list);
 
-        String[] petitions={"Petition 1","Petition 2","Petition 3"};
-        ListAdapter petitionAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,petitions);
+        ArrayList<String> petitionsList = new ArrayList<String>();
+        petitionsList.add("Petition 1");
+        petitionsList.add("Petition 2");
+        petitionsList.add("Petition 3");
+        ArrayList<String> petitionNumList = new ArrayList<String>();
+        petitionNumList.add("1.");
+        petitionNumList.add("2.");
+        petitionNumList.add("3.");
+        ArrayList<String> petitionVotesList = new ArrayList<String>();
+        petitionVotesList.add("5 votes");
+        petitionVotesList.add("10 votes");
+        petitionVotesList.add("15 votes");
+        ArrayList<String> timeAgoList = new ArrayList<String>();
+        timeAgoList.add(TimeAgo.toDuration(1230000000));
+        timeAgoList.add(TimeAgo.toDuration(1230000000));
+        timeAgoList.add(TimeAgo.toDuration(1230000000));
+        String[] petitions=petitionsList.toArray(new String[petitionsList.size()]);//{"Petition 1","Petition 2","Petition 3"};
+        String[] petitionNum=petitionNumList.toArray(new String[petitionNumList.size()]);//{"1.","2.","3."};
+        String[] petitionVotes=petitionVotesList.toArray(new String[petitionVotesList.size()]);//{"5 votes","10 votes","15 votes"};
+        String[] timeAgo=timeAgoList.toArray(new String[timeAgoList.size()]);//{"5 days ago","10 days ago","15 days ago"};
+        ListAdapter petitionAdapter=new CustomAdapter(this,petitions,petitionVotes,petitionNum,timeAgo);
         ListView petitionsListView=(ListView) findViewById(R.id.petitionListView);
         petitionsListView.setAdapter(petitionAdapter);
+
+
+
+
 
         petitionsListView.setOnItemClickListener(
                 new AdapterView.OnItemClickListener() {
