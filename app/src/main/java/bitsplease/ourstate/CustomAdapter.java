a8a5462feb59de62378c825bee35c4aee24f9edb
Armandh96 <armandh@iastate.edu>
2016-09-17 20:47:01 -0500

    Fixed some shit

---
+++ app/src/main/java/bitsplease/ourstate/CustomAdapter.java
@@ -0,0 +1,11 @@
+package bitsplease.ourstate;
+
+/**
+ * Created by Armand on 9/17/2016.
+ */
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+public class CustomAdapter {
+}
